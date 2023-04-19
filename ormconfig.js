3ce126ed139944c79d32a325cb51b50dfd010d5b
Metizsoft Solutions Private Limited <metizsoft@gmail.com>
2023-04-19 19:43:07 +0530

    Add files via upload

---
+++ ormconfig.js
@@ -0,0 +1,6 @@
+const AdminUser = require('nestjs-admin').AdminUserEntity
+module.exports = {
+  entities: [/* ... */, AdminUser],
+  // Alternatively:
+  // entities: [/* ... */, 'node_modules/nestjs-admin/**/*.entity.js'],
+}
